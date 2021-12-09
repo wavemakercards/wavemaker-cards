a158e400680f6fb7c12ffa1b291ec23d4d94b85d
iainwoodou <iain.wood@open.ac.uk>
2021-12-09 18:42:36 +0000

    dist created

---
+++ dist/precache-manifest.88a22aabb50aa702db9420d78bfd3322.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05dc627b48d4d4b88e3a",
+    "url": "css/app.b3a972a1.css"
+  },
+  {
+    "revision": "0a342b2824e3d9d95170",
+    "url": "css/chunk-7d330077.76a811a3.css"
+  },
+  {
+    "revision": "af315b6ff8b10d8608b9",
+    "url": "css/chunk-vendors.802d48a6.css"
+  },
+  {
+    "revision": "ab41f391445cb3014e9e159d9fae91d9",
+    "url": "css/wavemaker.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "06bc955d2ddace0edfd78a736d76d6ea",
+    "url": "icons/icon.png"
+  },
+  {
+    "revision": "175167a6c3225269aa78f26cce4936ba",
+    "url": "index.html"
+  },
+  {
+    "revision": "05dc627b48d4d4b88e3a",
+    "url": "js/app.bca79a4c.js"
+  },
+  {
+    "revision": "0a342b2824e3d9d95170",
+    "url": "js/chunk-7d330077.f58da89d.js"
+  },
+  {
+    "revision": "3bfc74c96be7b8a1e3f6",
+    "url": "js/chunk-d8e602e4.966fd43d.js"
+  },
+  {
+    "revision": "af315b6ff8b10d8608b9",
+    "url": "js/chunk-vendors.d91ad5fd.js"
+  },
+  {
+    "revision": "e8888bc56496f2dfb58257745a26039d",
+    "url": "js/wavemaker.js"
+  },
+  {
+    "revision": "787e6e95d122441a162aa316019007dd",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
