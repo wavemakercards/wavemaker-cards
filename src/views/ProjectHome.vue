07ae22ae5458bf533c1845a2b2b52f31e88706e2
Iain Wood <cards@wavemaker.co.uk>
2021-12-11 10:41:45 +0000

    minor changes

--- src/views/ProjectHome.vue
+++ src/views/ProjectHome.vue
@@ -2,7 +2,15 @@
 <div>
 <h1>Project</h1>
 <label>Project Title</label>
-<input type="text" placeholder="Project Title" v-model="$root.shadowDB.Settings[Object.keys($root.shadowDB.Settings)[0]].settings.title" />
+<input type="text" placeholder="Project Title"/>
+
+<v-text-field
+                v-model="$root.shadowDB.Settings[Object.keys($root.shadowDB.Settings)[0]].settings.title" 
+                placeholder="Project Title"
+                  label="Project Title"
+                  @blur="SaveChange()"
+                ></v-text-field>
+
 <p>
 I'm sure there will be more stuff here but I'm blanking right now - theme, font stuff maybe some stats
 <span class="material-icons">face</span>
@@ -16,6 +24,16 @@ I'm sure there will be more stuff here but I'm blanking right now - theme, font 
 export default ({
  beforeMount(){
      this.$root.showNavigation =  true
+ },
+ methods: {
+     SaveChange() {
+      this.$root.UpdateRecord(
+        "Settings",
+        Object.keys(this.$root.shadowDB.Settings)[0],
+        this.$root.shadowDB.Settings[Object.keys(this.$root.shadowDB.Settings)[0]]
+      );
+      
+    }
  }
 })
 </script>
